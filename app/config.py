71bf3f09be28890b8f794de17d3925a347ae2086
Kamil Biktimirov <Biktimirov.kamil@yandex.ru>
2025-09-10 15:41:19 +0300

    refactor(config): removed unused configs

--- app/config.py
+++ app/config.py
@@ -2,27 +2,14 @@
 
 from __future__ import annotations
 
-from pydantic import Field, SecretStr
+from pydantic import Field
 
-from app.lib.config.base import BaseConfig, BaseSettings
+from app.lib.config.base import BaseConfig
 from app.lib.config.idempotency import IdempotencyConfig
 from app.lib.config.redis import RedisConfig
 from app.lib.config.server import ServerConfig
 from app.lib.config.sqlalchemy import SQLAlchemyConfig
 from app.lib.observability.config import ObservabilityConfig
-
-
-class SecretsConfig(BaseSettings):
-    """Secrets config."""
-
-    encryption_key: SecretStr
-    api_key: SecretStr
-
-
-class GetlateConfig(BaseSettings):
-    """Getlate config."""
-
-    api_key: SecretStr
 
 
 # MUST ALWAYS BE LAST
@@ -31,8 +18,6 @@ class AppConfig(BaseConfig):
 
     observability: ObservabilityConfig = Field(default_factory=ObservabilityConfig)
     database: SQLAlchemyConfig
-    secrets: SecretsConfig
     redis: RedisConfig = Field(default_factory=RedisConfig)
     server: ServerConfig = Field(default_factory=ServerConfig)
     idempotency: IdempotencyConfig = Field(default_factory=IdempotencyConfig)
-    getlate: GetlateConfig
